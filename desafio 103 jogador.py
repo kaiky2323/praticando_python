2ff9d64b7a6a91798ebb0c1e46d728a7ac55ba22
kaiky2323 <teodorokaiky2@gmail.com>
2025-08-19 23:35:32 -0300

    desafio 103

--- desafio 103 jogador.py
+++ desafio 103 jogador.py
@@ -1,14 +1,18 @@
 '''Faça um programa que tenha uma função chamada ficha(),
 que receba dois parâmetros opcionais: o nome de um jogador e quantos
 gols ele marcou. O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.'''
-import math
-def ficha(nome, gol):
-    if nome.strip()== '':
-        nome = 'Desconhecido'
-    if gol.isnumeric():
-        gol = int(gol)
-    else:
-        gol = 0
-    return f'O jogador {nome} fez {gol} gol(s)'
 
-print(ficha(nome=str(input('Nome do jogador: ')), gol=input('gol(s): ')))
+def ficha (jogador = 'Desconhecido', gol = 0):
+    print(f'O jogador {jogador} fez {gol} gols.')
+
+jog = str(input('Nome do jogador: '))
+g = str(input('Quantidade de gols: '))
+
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if jog.strip() == '':
+    ficha(gol= g)
+else:
+    ficha(jog, g)
