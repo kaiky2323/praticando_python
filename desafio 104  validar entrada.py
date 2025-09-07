8ffba85591137badced54bd101d79ff954694890
kaiky2323 <teodorokaiky2@gmail.com>
2025-09-07 17:34:09 -0300

    novos desafios

--- desafio 104  validar entrada.py
+++ desafio 104  validar entrada.py
@@ -1,10 +1,18 @@
 def leiaint(num):
+    ok = False
+    valor = 0
+    while True:
+        numero = str(input(num))
+        if numero.isnumeric():
+            ok = True
+            valor = int(numero)
+        else:
+            print('\033[31mERRO! digite um numero inteiro valido\033[m')
+        if ok:
+            break
+    return valor
 
 
-#Programa principal
-n = leiaint('Digite um numero: '))
-print(f'Você digitou o numero {n}')
-
-
-
-
+#programa principal
+numero = leiaint('Digite um numero: ')
+print(f'Você acabou de digitar o valor {numero}')
---
+++ desafio105_situaçãonota.py
@@ -0,0 +1,26 @@
+def lnota(*notas, situacao = False):
+
+    '''ao declarar que a situação = True
+    mostra a situação da turma: Boa, Razoavel, Ruim'''
+
+
+    resultado = {
+        'total' : len(notas),
+        'media' : sum(notas)/len(notas),
+        'maior' : max(notas),
+        'menor' : min(notas)
+    }
+    if situacao:
+        if resultado['media'] > 7:
+            resultado['situacao'] = 'Boa'
+        elif resultado['media'] >= 5:
+            resultado['situacao'] = 'Razoavel'
+        else:
+            resultado['situacao'] = 'Ruim'
+
+    return resultado
+
+#Programa Principal
+resposta = lnota(4, 7, 10, 5.5, 4, situacao=True)
+print(resposta)
+lnota(help())
